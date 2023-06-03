961bf310baef3028f7084264245afa6c7aa63d55
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-06-03 11:47:59 +0530

    Add. Added the predict part of the application

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
-from flask import Flask,render_template
+from tensorflow import keras
+import base64,os,cv2
+from flask import Flask, render_template, request, url_for, redirect,session
+from werkzeug.utils import secure_filename
+import numpy as np
 
+app = Flask(__name__, template_folder='templates', static_folder='static')
+UPLOAD_FOLDER = os.path.join('static', 'images')
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'EDCBA'
 
-app = Flask(__name__)
+@app.route('/preview', methods=['GET','POST'])
+def preview():
+    img_file_path = session.get('uploaded_img_file_path', None)
+    model = keras.models.load_model('./model/model.h5')
+    categories = ["NonDemented", "MildDemented", "ModerateDemented", "VeryMildDemented"]
+    images = []
+    data = cv2.imread(img_file_path,cv2.IMREAD_GRAYSCALE)
+    new_data = cv2.resize(data,(120,120),interpolation=cv2.INTER_AREA)
+    new_data = new_data / 255.0
+    images.append(new_data)
+    ptitle = str()
+    for img in images:
+        image = np.array(img).reshape(-1,120,120)
+        prediction = model.predict(image)
+        ptitle = "Prediction: {0}".format(categories[np.argmax(prediction)])
+        print(ptitle)
+    return render_template("preview.html",image=img_file_path,result=ptitle)
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        image = request.files['image']
+        img_filename = secure_filename(image.filename)
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
+        print(image)
+        session['uploaded_img_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
+        return redirect("/preview")
+    return render_template("predict.html")
 
 @app.route('/login')
 def login():
